b9530d6ecd38a16dd7f0330f29b5ef5bb201bada
PhanTien234 <minhtienp328@gmail.com>
2023-09-21 00:18:21 +0700

    Validate for express

--- app/src/main/java/com/example/exercise_1_calculator/MainActivity.java
+++ app/src/main/java/com/example/exercise_1_calculator/MainActivity.java
@@ -1,79 +1,128 @@
 package com.example.exercise_1_calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.button.MaterialButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 
-    TextView resultTV, solutionTV;
-    MaterialButton buttonC, buttonBrackOpen, buttonBrackClose;
-    MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
-    MaterialButton button0, button1,button2, button3, button4, button5, button6, button7, button8, button9;
-    MaterialButton buttonAC, buttonDot;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView resultTv,solutionTv;
+    MaterialButton buttonC,buttonBrackOpen,buttonBrackClose;
+    MaterialButton buttonDivide,buttonMultiply,buttonPlus,buttonMinus,buttonEquals;
+    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
+    MaterialButton buttonAC,buttonDot;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resultTV = findViewById(R.id.result_tv);
-        solutionTV = findViewById(R.id.solution_tv);
+        resultTv = findViewById(R.id.result_tv);
+        solutionTv = findViewById(R.id.solution_tv);
 
-        assignId(buttonC, R.id.button_C);
-        assignId(buttonBrackOpen, R.id.button_open_bracket);
-        assignId(buttonBrackClose, R.id.button_close_bracket);
-        assignId(buttonDivide, R.id.button_divide);
-        assignId(buttonMultiply, R.id.button_multiply);
-        assignId(buttonPlus, R.id.button_plus);
-        assignId(buttonMinus, R.id.button_minus);
-        assignId(buttonEquals, R.id.button_equals);
-        assignId(button0, R.id.button_0);
-        assignId(button1, R.id.button_1);
-        assignId(button2, R.id.button_2);
-        assignId(button3, R.id.button_3);
-        assignId(button4, R.id.button_4);
-        assignId(button5, R.id.button_5);
-        assignId(button6, R.id.button_6);
-        assignId(button7, R.id.button_7);
-        assignId(button8, R.id.button_8);
-        assignId(button9, R.id.button_9);
-        assignId(buttonAC, R.id.button_AC);
-        assignId(buttonDot, R.id.button_dot);
+        assignId(buttonC,R.id.button_C);
+        assignId(buttonBrackOpen,R.id.button_open_bracket);
+        assignId(buttonBrackClose,R.id.button_close_bracket);
+        assignId(buttonDivide,R.id.button_divide);
+        assignId(buttonMultiply,R.id.button_multiply);
+        assignId(buttonPlus,R.id.button_plus);
+        assignId(buttonMinus,R.id.button_minus);
+        assignId(buttonEquals,R.id.button_equals);
+        assignId(button0,R.id.button_0);
+        assignId(button1,R.id.button_1);
+        assignId(button2,R.id.button_2);
+        assignId(button3,R.id.button_3);
+        assignId(button4,R.id.button_4);
+        assignId(button5,R.id.button_5);
+        assignId(button6,R.id.button_6);
+        assignId(button7,R.id.button_7);
+        assignId(button8,R.id.button_8);
+        assignId(button9,R.id.button_9);
+        assignId(buttonAC,R.id.button_AC);
+        assignId(buttonDot,R.id.button_dot);
     }
 
-    void assignId(MaterialButton btn, int id){
+    void assignId(MaterialButton btn,int id){
         btn = findViewById(id);
         btn.setOnClickListener(this);
     }
+
 
     @Override
     public void onClick(View view) {
         MaterialButton button = (MaterialButton) view;
         String buttonText = button.getText().toString();
-        solutionTV.setText(buttonText);
-        String dataToCalculate = solutionTV.getText().toString();
+        String dataToCalculate = solutionTv.getText().toString();
 
-        if(buttonText.equals("AC")){
-            solutionTV.setText("");
-            resultTV.setText("0");
+        if (buttonText.equals("AC")) {
+            solutionTv.setText("0");
+            resultTv.setText("0");
             return;
         }
-        if(buttonText.equals("=")){
-            solutionTV.setText(resultTV.getText());
+        if (buttonText.equals("=")) {
+            // Only update resultTv when "=" is pressed.
+            String result = getResult(dataToCalculate);
+
+            if (!result.equals("Err")) {
+                resultTv.setText(result);
+                solutionTv.setText(result); // Set solutionTv to the result as well.
+            }
             return;
         }
-        if(buttonText.equals("C")){
-            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() -1);
-        }else {
-            dataToCalculate = dataToCalculate + buttonText;
+        if (buttonText.equals("C")) {
+            if (!dataToCalculate.isEmpty()) {
+                dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
+            }
         }
-        solutionTV.setText(dataToCalculate);
+        else {
+            // Check if the current text starts with "0" and remove the leading "0" if necessary.
+            if (dataToCalculate.equals("0")) {
+                dataToCalculate = buttonText;
+            } else {
+                dataToCalculate = dataToCalculate + buttonText;
+            }
+        }
+        solutionTv.setText(dataToCalculate);
 
+//        // Check if dataToCalculate is empty before attempting evaluation.
+//        if (!dataToCalculate.isEmpty()) {
+//
+//
+////            String finalResult = getResult(dataToCalculate);
+////
+////            if (!finalResult.equals("Err")) {
+////                resultTv.setText(finalResult);
+////            }
+//        }
+        if(dataToCalculate.isEmpty()) {
+            // If dataToCalculate is empty, set resultTv to "0".
+            solutionTv.setText("0");
+            resultTv.setText("0");
+        }
     }
+
+
     String getResult(String data){
-        return "Calculated"
+        try{
+            Context context  = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult =  context.evaluateString(scriptable,data,"Javascript",1,null).toString();
+            if(finalResult.endsWith(".0")){
+                finalResult = finalResult.replace(".0","");
+            }
+            return finalResult;
+        }catch (Exception e){
+            return "Err";
+        }
     }
+
 }
